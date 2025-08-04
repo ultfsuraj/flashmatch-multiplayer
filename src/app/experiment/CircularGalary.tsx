4a45f36c93b967603dbc16229b101563e51cc5bd
R B suraj <rbsuraj432@gmail.com>
2025-08-04 21:52:13 +0530

    icon placed and will test controller

--- src/app/experiment/CircularGalary.tsx
+++ src/app/experiment/CircularGalary.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion, useMotionValue } from 'motion/react';
+import { motion, useAnimation } from 'motion/react';
 import { useEffect, useRef, useState } from 'react';
 
 const getRadius = (w: number, h: number) => {
@@ -11,13 +11,13 @@ const getRadius = (w: number, h: number) => {
 const CircularGalary = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const div1Ref = useRef<HTMLDivElement>(null);
+  const controller = useAnimation();
+
   const [r, setR] = useState(0);
   const [w, setW] = useState(0);
   const [h, setH] = useState(0);
-
-  const circX = (extra: number) => r * Math.cos((extra * Math.PI) / 180) + r - (div1Ref.current?.offsetWidth || 0) / 2;
-
-  const circY = (extra: number) => r * Math.sin((extra * Math.PI) / 180) + r - (div1Ref.current?.offsetHeight || 0) / 2;
+  const [chordAngle, setChordAngle] = useState(0);
+  const [POSITIONS, setPOSITIONS] = useState<{ x: number; y: number; scale: number }[]>([]);
 
   useEffect(() => {
     const width = containerRef.current?.offsetWidth || 0;
@@ -25,12 +25,57 @@ const CircularGalary = () => {
     setR(getRadius(width, height));
     setW(width);
     setH(height);
+    setChordAngle(2 * Math.atan(height / 2 / (getRadius(width, height) - width)) * (180 / Math.PI));
   }, []);
+
+  useEffect(() => {
+    setPOSITIONS([
+      {
+        x: circX(-chordAngle / 2 - 1),
+        y: circY(-chordAngle / 2 - 1),
+        scale: 0.3,
+      },
+      {
+        x: circX(-(23 * (chordAngle / 2)) / 30),
+        y: circY(-(23 * (chordAngle / 2)) / 30),
+        scale: 0.5,
+      },
+      {
+        x: circX(-(13 * (chordAngle / 2)) / 30),
+        y: circY(-(13 * (chordAngle / 2)) / 30),
+        scale: 0.7,
+      },
+      {
+        x: circX(0),
+        y: circY(0),
+        scale: 1,
+      },
+      {
+        x: circX((13 * (chordAngle / 2)) / 30),
+        y: circY((13 * (chordAngle / 2)) / 30),
+        scale: 0.7,
+      },
+      {
+        x: circX((23 * (chordAngle / 2)) / 30),
+        y: circY((23 * (chordAngle / 2)) / 30),
+        scale: 0.5,
+      },
+      {
+        x: circX(chordAngle / 2 + 1),
+        y: circY(chordAngle / 2 + 1),
+        scale: 0.3,
+      },
+    ]);
+  }, [chordAngle]);
+
+  const circX = (extra: number) => r * Math.cos((extra * Math.PI) / 180) + r - (div1Ref.current?.offsetWidth || 0) / 2;
+  const circY = (extra: number) => r * Math.sin((extra * Math.PI) / 180) + r - (div1Ref.current?.offsetHeight || 0) / 2;
+  const handleDragEnd = () => {};
 
   return (
     <div ref={containerRef} className="mt-[5vh] h-[90vh] w-[70vw] bg-zinc-300">
       <motion.div
-        className="relative rounded-full bg-amber-200"
+        className="relative rounded-full bg-teal-200"
         style={{
           height: 2 * r,
           width: 2 * r,
@@ -38,46 +83,38 @@ const CircularGalary = () => {
           y: -10,
         }}
       >
+        {/* controller  */}
         <motion.div
-          className="absolute h-32 w-32 rounded-full bg-zinc-700"
-          style={{
-            x: circX(-55),
-            y: circY(-55),
-            scale: 0.6,
-          }}
-        ></motion.div>
-        <motion.div
-          className="absolute h-32 w-32 rounded-full bg-zinc-700"
-          style={{
-            x: circX(-30),
-            y: circY(-30),
-            scale: 0.8,
-          }}
-        ></motion.div>
-        <motion.div
-          className="absolute h-32 w-32 rounded-full bg-zinc-700"
+          className="absolute h-32 w-32 rounded-full border border-zinc-800 bg-transparent"
           ref={div1Ref}
           style={{
             x: circX(0),
             y: circY(0),
+            scale: 1.2,
           }}
         ></motion.div>
-        <motion.div
-          className="absolute h-32 w-32 rounded-full bg-zinc-700"
-          style={{
-            x: circX(30),
-            y: circY(30),
-            scale: 0.8,
-          }}
-        ></motion.div>
-        <motion.div
-          className="absolute h-32 w-32 rounded-full bg-zinc-700"
-          style={{
-            x: circX(55),
-            y: circY(55),
-            scale: 0.6,
-          }}
-        ></motion.div>
+
+        {/* items */}
+
+        {POSITIONS.map((pos, index) => (
+          <motion.div
+            key={index}
+            className="absolute h-32 w-32 rounded-full bg-zinc-700"
+            style={{
+              x: pos.x,
+              y: pos.y,
+              scale: pos.scale,
+            }}
+            animate={index === 3 ? controller : {}}
+            drag="y"
+            dragConstraints={{ top: pos.y, bottom: pos.y }}
+            dragElastic={0.03}
+            whileTap={{ scale: 0.95 }}
+            onDragEnd={(event, info) => {
+              console.log(info.velocity.y > 0 ? 'down' : 'up');
+            }}
+          ></motion.div>
+        ))}
       </motion.div>
     </div>
   );
