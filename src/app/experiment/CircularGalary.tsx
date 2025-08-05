f6ec28a6b4c4db155c7f475863922acfffbcb4c7
R B suraj <rbsuraj432@gmail.com>
2025-08-05 10:49:04 +0530

    bug: rotation happening fine, but color didn't change and onclick only the id changes but other props are same , check console

--- src/app/experiment/CircularGalary.tsx
+++ src/app/experiment/CircularGalary.tsx
@@ -11,69 +11,106 @@ const getRadius = (w: number, h: number) => {
 const CircularGalary = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const div1Ref = useRef<HTMLDivElement>(null);
-  const controller = useAnimation();
+
+  const controlsARR = useRef([
+    useAnimation(),
+    useAnimation(),
+    useAnimation(),
+    useAnimation(),
+    useAnimation(),
+    useAnimation(),
+    useAnimation(),
+    useAnimation(),
+  ]);
 
   const [r, setR] = useState(0);
   const [w, setW] = useState(0);
   const [h, setH] = useState(0);
   const [chordAngle, setChordAngle] = useState(0);
-  const [POSITIONS, setPOSITIONS] = useState<{ x: number; y: number; scale: number }[]>([]);
+  const [POSITIONS, setPOSITIONS] = useState<{ id: number; x: number; y: number; scale: number; color: string }[]>([]);
 
   useEffect(() => {
     const width = containerRef.current?.offsetWidth || 0;
     const height = containerRef.current?.offsetHeight || 0;
-    setR(getRadius(width, height));
+    const radius = getRadius(width, height);
+    setR(radius);
     setW(width);
     setH(height);
-    setChordAngle(2 * Math.atan(height / 2 / (getRadius(width, height) - width)) * (180 / Math.PI));
+    setChordAngle(2 * Math.atan(height / 2 / (radius - width)) * (180 / Math.PI));
   }, []);
 
   useEffect(() => {
     setPOSITIONS([
+      { id: 0, x: circX(-chordAngle / 2 - 1), y: circY(-chordAngle / 2 - 1), scale: 0.25, color: '#ffe6d2' },
       {
-        x: circX(-chordAngle / 2 - 1),
-        y: circY(-chordAngle / 2 - 1),
-        scale: 0.3,
-      },
-      {
+        id: 1,
         x: circX(-(23 * (chordAngle / 2)) / 30),
         y: circY(-(23 * (chordAngle / 2)) / 30),
-        scale: 0.5,
+        scale: 0.45,
+        color: '#cc66cc',
       },
       {
+        id: 2,
         x: circX(-(13 * (chordAngle / 2)) / 30),
         y: circY(-(13 * (chordAngle / 2)) / 30),
         scale: 0.7,
+        color: '#ffb400',
       },
+      { id: 3, x: circX(0), y: circY(0), scale: 1, color: '#c73b32' },
       {
-        x: circX(0),
-        y: circY(0),
-        scale: 1,
-      },
-      {
+        id: 4,
         x: circX((13 * (chordAngle / 2)) / 30),
         y: circY((13 * (chordAngle / 2)) / 30),
         scale: 0.7,
+        color: '#32b4ff',
       },
       {
+        id: 5,
         x: circX((23 * (chordAngle / 2)) / 30),
         y: circY((23 * (chordAngle / 2)) / 30),
-        scale: 0.5,
+        scale: 0.45,
+        color: '#995f3d',
       },
-      {
-        x: circX(chordAngle / 2 + 1),
-        y: circY(chordAngle / 2 + 1),
-        scale: 0.3,
-      },
+      { id: 6, x: circX(chordAngle / 2 + 1), y: circY(chordAngle / 2 + 1), scale: 0.25, color: '#0ac832' },
+      { id: 7, x: circX(chordAngle), y: circY(chordAngle), scale: 0.1, color: '#1e2832' },
     ]);
   }, [chordAngle]);
 
   const circX = (extra: number) => r * Math.cos((extra * Math.PI) / 180) + r - (div1Ref.current?.offsetWidth || 0) / 2;
   const circY = (extra: number) => r * Math.sin((extra * Math.PI) / 180) + r - (div1Ref.current?.offsetHeight || 0) / 2;
-  const handleDragEnd = () => {};
+
+  const handleDragEnd = async (down: boolean) => {
+    const len = POSITIONS.length;
+
+    await Promise.all(
+      controlsARR.current.map((control, index) =>
+        control.start({
+          ...POSITIONS[(index + (down ? 1 : -1) + len) % len],
+          transition: { duration: 0.4, ease: 'easeInOut' },
+        })
+      )
+    );
+
+    const newPositions = JSON.parse(JSON.stringify(POSITIONS));
+    if (down) {
+      newPositions.push(newPositions[0]);
+      newPositions.splice(0, 1);
+      for (let i = 0; i < len; i++) {
+        newPositions[i].id = i;
+      }
+    } else {
+      newPositions.unshift(newPositions[len - 1]);
+      newPositions.pop();
+      for (let i = 0; i < len; i++) {
+        newPositions[i].id = i;
+      }
+    }
+    setPOSITIONS(newPositions);
+    console.log(newPositions);
+  };
 
   return (
-    <div ref={containerRef} className="mt-[5vh] h-[90vh] w-[70vw] bg-zinc-300">
+    <div ref={containerRef} className="mt-[7vh] h-[85vh] w-[65vw]">
       <motion.div
         className="relative rounded-full bg-teal-200"
         style={{
@@ -98,20 +135,23 @@ const CircularGalary = () => {
 
         {POSITIONS.map((pos, index) => (
           <motion.div
-            key={index}
+            key={pos.id}
             className="absolute h-32 w-32 rounded-full bg-zinc-700"
             style={{
               x: pos.x,
               y: pos.y,
               scale: pos.scale,
+              backgroundColor: pos.color,
             }}
-            animate={index === 3 ? controller : {}}
+            animate={controlsARR.current[pos.id]}
             drag="y"
             dragConstraints={{ top: pos.y, bottom: pos.y }}
             dragElastic={0.03}
-            whileTap={{ scale: 0.95 }}
             onDragEnd={(event, info) => {
-              console.log(info.velocity.y > 0 ? 'down' : 'up');
+              handleDragEnd(info.velocity.y > 0);
+            }}
+            onClick={() => {
+              console.log(pos);
             }}
           ></motion.div>
         ))}
